3cb0331f7b008413a23db662650200d5d06a1535
Slava Andreyev <sandreyev@ets.org>
2018-10-23 16:50:50 -0400

    updated usage comments

--- src/common/scripts/vnc_startup.sh
+++ src/common/scripts/vnc_startup.sh
@@ -6,13 +6,10 @@ set -e
 help (){
 echo "
 USAGE:
-docker run -it -p 6901:6901 -p 5901:5901 consol/<image>:<tag> <option>
+docker run -it -p 5901:5901 consol/<image>:<tag> <option>
 
 IMAGES:
-consol/ubuntu-xfce-vnc
 consol/centos-xfce-vnc
-consol/ubuntu-icewm-vnc
-consol/centos-icewm-vnc
 
 TAGS:
 latest  stable version of branch 'master'
